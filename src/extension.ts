6998584ec9f6047ff23b2437d789fbdc27a6e47f
Mark Skelton <mdskelton99@gmail.com>
2025-06-04 13:44:35 -0500

    Icon and make it work

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,60 @@
-import * as vscode from 'vscode';
-import { spawn } from 'child_process';
+import * as vscode from 'vscode'
+import { spawn } from 'child_process'
 
 export function activate(context: vscode.ExtensionContext) {
   const formatter = vscode.languages.registerDocumentFormattingEditProvider(
     { scheme: 'file', language: 'dts' },
     {
-      async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-        const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
-        const cwd = workspaceFolder ? workspaceFolder.uri.fsPath : undefined;
+      async provideDocumentFormattingEdits(
+        document: vscode.TextDocument,
+      ): Promise<vscode.TextEdit[]> {
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(
+          document.uri,
+        )
+        const cwd = workspaceFolder ? workspaceFolder.uri.fsPath : undefined
 
         return new Promise((resolve, reject) => {
-          const documentText = document.getText();
-          const dtsfmt = spawn('dtsfmt', ['--stdin'], { cwd });
+          const documentText = document.getText()
+          const dtsfmt = spawn('dtsfmt', ['--stdin', document.uri.fsPath], {
+            cwd,
+          })
 
-          let stdout = '';
-          let stderr = '';
+          let stdout = ''
+          let stderr = ''
 
           dtsfmt.stdout.on('data', (data) => {
-            stdout += data.toString();
-          });
+            stdout += data.toString()
+          })
 
           dtsfmt.stderr.on('data', (data) => {
-            stderr += data.toString();
-          });
+            stderr += data.toString()
+          })
 
           dtsfmt.on('close', (code) => {
             if (code !== 0) {
-              vscode.window.showErrorMessage(`dtsfmt error: ${stderr}`);
-              resolve([]);
+              reject(new Error(`dtsfmt error: ${stderr}`))
             } else {
               const fullRange = new vscode.Range(
                 document.positionAt(0),
-                document.positionAt(documentText.length)
-              );
+                document.positionAt(documentText.length),
+              )
 
-              resolve([vscode.TextEdit.replace(fullRange, stdout)]);
+              resolve([vscode.TextEdit.replace(fullRange, stdout)])
             }
-          });
+          })
 
           dtsfmt.on('error', (error) => {
-            vscode.window.showErrorMessage(`dtsfmt error: ${error.message}`);
-            resolve([]);
-          });
+            reject(new Error(`dtsfmt error: ${error.message}`))
+          })
 
-          dtsfmt.stdin.write(documentText);
-          dtsfmt.stdin.end();
-        });
-      }
-    }
-  );
+          dtsfmt.stdin.write(documentText)
+          dtsfmt.stdin.end()
+        })
+      },
+    },
+  )
 
-  context.subscriptions.push(formatter);
+  context.subscriptions.push(formatter)
 }
 
 export function deactivate() {}
